6c013e27bd6d0a6266f5e7ef4b2d5028203155c2
JeremyPromsy <jeremypromsy@gmail.com>
2023-10-11 16:51:11 +0200

    Avancement

--- snapface/src/app/app.module.ts
+++ snapface/src/app/app.module.ts
@@ -4,24 +4,20 @@ import { AppComponent } from './app.component';
 import { registerLocaleData } from '@angular/common';
 import * as fr from '@angular/common/locales/fr';
 import { AppRoutingModule } from './app-routing.module';
-import { LandingPageComponent } from './landing-page/landing-page.component';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 import { CoreModule } from './core/core.module';
-import { FaceSnapsModule } from './face-snaps/face-snaps.module';
+import { AuthModule } from './auth/auth.module';
+import { LandingPageComponent } from './landing-page/components/landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule, 
-    FormsModule, 
-    HttpClientModule,
     CoreModule,
-    FaceSnapsModule,
+    LandingPageComponent,
+    AuthModule,
   ],
   bootstrap: [AppComponent]
 })
