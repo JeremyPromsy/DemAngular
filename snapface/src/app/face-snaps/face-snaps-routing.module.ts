5ae133260425381a94251676f3116e8d84fd1618
JeremyPromsy <jeremypromsy@gmail.com>
2023-10-12 10:57:52 +0200

    Finalisation

--- snapface/src/app/face-snaps/face-snaps-routing.module.ts
+++ snapface/src/app/face-snaps/face-snaps-routing.module.ts
@@ -3,12 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { FaceSnapListComponent } from './components/face-snap-list/face-snap-list.component';
 import { NewFaceSnapComponent } from './components/new-face-snap/new-face-snap.component';
+import { AuthGuard } from '../core/guards/auth.guard';
 import { SingleFaceSnapComponent } from './components/single-facesnap/single-face-snap.component';
 
 const routes: Routes = [
-    { path: 'create', component: NewFaceSnapComponent },
-    { path: ':id', component: SingleFaceSnapComponent },
-    { path: '', component: FaceSnapListComponent },
+  { path: 'create', component: NewFaceSnapComponent, canActivate: [AuthGuard] },
+  { path: ':id', component: SingleFaceSnapComponent, canActivate: [AuthGuard] },
+  { path: '', component: FaceSnapListComponent, canActivate: [AuthGuard] },
 ];
 
 @NgModule({
